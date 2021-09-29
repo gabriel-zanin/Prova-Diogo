ec94444ea8e7df690ab00c0185e8d0ad9aebf92a
cneres <58155380+cneres@users.noreply.github.com>
2021-09-29 18:32:13 -0300

    Implementação ServiceMusica ts

--- Prova-Diogo/FRONT/src/app/app.module.ts
+++ Prova-Diogo/FRONT/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
